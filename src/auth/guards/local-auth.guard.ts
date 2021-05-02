0495538600ef147bf00708c8b688225e19419634
James Rogers <jd2rogers2@gmail.com>
2021-05-02 13:52:19 -0700

    dev note

--- src/auth/guards/local-auth.guard.ts
+++ src/auth/guards/local-auth.guard.ts
@@ -1,5 +1,13 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+
+/*
+  dev note:
+  UseGuards(AuthGuard), or UseGuards(LocalAuthGuard) in our case,
+  calls this.canActivate (see AuthGuard class)
+  which calls validate in our stategy
+  we could write a custom canActivate here
+*/
 
 @Injectable()
 export class LocalAuthGuard extends AuthGuard('local') {}
