3cedebd01e013bbe74d8a9746268edb95b592c70
Josh <mrjoshbgoldstein@gmail.com>
2021-05-02 13:03:51 -0700

    adding react

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.enableCors();
 
     app.useStaticAssets(join(__dirname, '..', 'public'));
     app.setBaseViewsDir(join(__dirname, '..', 'views'));
