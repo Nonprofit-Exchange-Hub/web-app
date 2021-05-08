e8afa49a9955424a830bc56afccb10da92a9818f
James Rogers <jd2rogers2@gmail.com>
2021-05-08 10:42:39 -0700

    add jwt guard per nest docs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 
 import { LoginAuthGuard } from './guards/login-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private authService: AuthService) {}
+
     @Post('test')
     @UseGuards(LoginAuthGuard)
     async authTest() {
         return {
             message: 'ヽ(•‿•)ノ',
         };
+    }
+
+    @Post('login')
+    @UseGuards(LoginAuthGuard)
+    async login(@Request() req) {
+        return this.authService.createJwt(req.user);
     }
 }
