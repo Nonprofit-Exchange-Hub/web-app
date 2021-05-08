73229ba9ca5c7a5051f5ee4e4fad183f43a4fcbf
Josh <mrjoshbgoldstein@gmail.com>
2021-05-08 15:34:23 -0700

    merging

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Footer from "./Footer.js";
+import Header from "./Header.js";
+import logo from './logo.svg';
 import './App.css';
 
 import Routes from './Routes';
 
 function App() {
-	const [ data, setData ] = React.useState(null);
+	// const [ data, setData ] = React.useState(null);
 
-	React.useEffect(() => {
-		fetch('http://localhost:3001/user/8').then((res) => res.json()).then((data) => setData(JSON.stringify(data)));
-	}, []);
+	// React.useEffect(() => {
+	// 	fetch('http://localhost:3001/user/8').then((res) => res.json()).then((data) => setData(JSON.stringify(data)));
+	// }, []);
 
 	return (
 		<BrowserRouter>
 			<div className="App">
-				<Routes />
+        <Header />
+				  <Routes />
+        <Footer />
 			</div>
 		</BrowserRouter>
 	);
