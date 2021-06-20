8c1ac59a0723c33a467f83fc9a486b639df8e1e6
James Rogers <jd2rogers2@gmail.com>
2021-06-20 11:47:33 -0700

    test build command

--- server/dist/auth/guards/local-auth.guard.d.ts
+++
@@ -1,4 +0,0 @@
-declare const LoginAuthGuard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
-export declare class LoginAuthGuard extends LoginAuthGuard_base {
-}
-export {};
--- server/dist/users.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { UserService } from './user.service';
-import { User } from './user.entity';
-export declare class UsersController {
-    private serv;
-    constructor(serv: UserService);
-    create(res: any, firstName: any, lastName: any): Promise<any>;
-    getById(params: any): Promise<User>;
-    get(id: any): Promise<User>;
-}
