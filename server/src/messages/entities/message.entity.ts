e59214798561d9b675a45b51f3de278970e35f0d
Ynda Jas <yndajas@gmail.com>
2021-10-15 21:20:56 +0100

    add messages entity

---
+++ server/src/messages/entities/message.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  CreateDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+// import { Transaction } from '../../transactions/entities/transaction.entity'
+
+@Entity('messages')
+export class Message {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  text: text;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  created_date: Date;
+
+  @ManyToOne(() => User, (user) => user.messages)
+  user: User;
+
+  // code below (and import above) for when Transaction is set up
+  // @ManyToOne(() => Transaction, transaction => transaction.messages)
+  // transaction: Transaction
+
+  @Column('int')
+  transaction_id: number;
+}
