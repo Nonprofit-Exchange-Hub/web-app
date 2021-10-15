2befeab15d66257b878db86cf9a1cdbb5c3a99a5
Ynda Jas <yndajas@gmail.com>
2021-10-15 21:25:34 +0100

    add message relationship to user entity

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,19 +1,23 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Message } from '../../messages/entities/message.entity.ts';
 
 @Entity('users')
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column('text')
-    first_name: string;
-  
-    @Column('text')
-    last_name: string;
-  
-    @Column({type:'text', unique: true})
-    email: string;
-  
-    @Column('text')
-    password: string;
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  first_name: string;
+
+  @Column('text')
+  last_name: string;
+
+  @Column({ type: 'text', unique: true })
+  email: string;
+
+  @Column('text')
+  password: string;
+
+  @OneToMany(() => Message, (message) => message.user)
+  messages: Message[];
 }
