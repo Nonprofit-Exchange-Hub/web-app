c690f38d544934a5f4d79aff3a6accb1ebb970cc
Ynda Jas <yndajas@gmail.com>
2021-10-20 00:39:24 +0100

    add messages module to app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,23 +6,24 @@ import { DatabaseConnectionService } from './database-connection.service';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MessagesModule } from './messages/messages.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 
-
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            envFilePath: '.env'
-        }),
-        TypeOrmModule.forRootAsync({ useClass: DatabaseConnectionService }),
-        UsersModule,
-        AuthModule,
-        ServeStaticModule.forRoot({
-            rootPath: join(__dirname, '../..', 'client/build'),
-        }),
-    ],
-    controllers: [ AppController ],
-    providers: []
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({ useClass: DatabaseConnectionService }),
+    UsersModule,
+    MessagesModule,
+    AuthModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'client/build'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [],
 })
 export class AppModule {}
