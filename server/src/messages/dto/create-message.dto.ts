43e0dd37ec58d8e646431198ea3d672eb5d137a2
Ynda Jas <yndajas@gmail.com>
2021-10-20 00:44:23 +0100

    remove date from messages dto - use default

--- server/src/messages/dto/create-message.dto.ts
+++ server/src/messages/dto/create-message.dto.ts
@@ -2,7 +2,6 @@ import { PartialType } from '@nestjs/mapped-types';
 import { Message } from '../entities/message.entity';
 
 export class CreateMessageDto extends PartialType(Message) {
-  text: text;
-  created_date: Date;
-  transaction_id: number; // remove one Transaction is set up and a relationship is used in the entity
+  text: string;
+  transaction_id: number; // remove once Transaction is set up and a relationship is used in the entity
 }
