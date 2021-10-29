ecbfe84b8187742f6ab2b54495317373f52c7761
dbrandt1990 <dvbrandt90@gmail.com>
2021-10-29 11:06:24 -0600

    FE signup divergence page

--- client/src/Signup.tsx
+++ client/src/Signup.tsx
@@ -4,8 +4,6 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Container, Button } from '@material-ui/core';
 import type { Theme } from '@material-ui/core/styles';
-// import GroupsIcon from '@mui/icons-material/Groups';
-// import PersonIcon from '@mui/icons-material/Person';
 
 const useStyles = makeStyles((theme: Theme) => ({
     imageBackground: {
@@ -34,38 +32,38 @@ const useStyles = makeStyles((theme: Theme) => ({
         textAlign: 'center'
     },
     positionOptions: {
-        marginLeft:'20%',
+        marginLeft: '20%',
         marginTop: '5rem'
     }
 }));
 
 function Signup() {
+
     const classes = useStyles()
+
     return (
-        <Container className='formBox'>
+        <Container className={classes.formBox}>
             <Typography variant="h2" component="h2">Welcome!</Typography>
-            <Typography variant="h2" component="h2">Select your account type.</Typography>            
-            <span>Already have an account? <Link to="/login">Log In</Link> </span>         
-           
+            <Typography variant="h2" component="h2">Select your account type.</Typography>
+            <Typography variant="body1" component="caption">Already have an account? <Link to="/login">Log In</Link> </Typography>
+
             <Grid container spacing={8}>
                 <Grid item className={classes.positionOptions}>
                     <div className={classes.centerText}>
                         <div className={classes.imageBackground}></div>
-                        <span>Are you a non-profit</span><br/>
-                        <span>organization?</span><br/>
-                    <Link className={classes.linkText} to="/signup_nonprofit">
-                        <Button variant='contained'>Create non-profit account</Button>
-                    </Link>
+                        <Typography variant="body1" component="caption">Are you a non-profit organization?</Typography>
+                        <Link className={classes.linkText} to="/signup_nonprofit">
+                            <Button variant="contained">Create non-profit account</Button>
+                        </Link>
                     </div>
                 </Grid>
                 <Grid item className={classes.positionOptions}>
                     <div className={classes.centerText}>
                         <div className={classes.imageBackground}></div>
-                        <span>Are you an individual</span><br/>
-                        <span>citizen?</span><br/>
-                    <Link className={classes.linkText} to="/signup_citizen">
-                        <Button variant='contained'>Create citizen account</Button>
-                    </Link>
+                        <Typography variant="body1" component="caption">Are you an individual citizen?</Typography>
+                        <Link className={classes.linkText} to="/signup_citizen">
+                            <Button variant="contained">Create citizen account</Button>
+                        </Link>
                     </div>
                 </Grid>
             </Grid>
