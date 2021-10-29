1ff786030242fcebbe13827ebf1b1b8972a9f39d
dbrandt1990 <dvbrandt90@gmail.com>
2021-10-29 11:41:01 -0600

    updated styles, applied material UI elements

--- client/src/Signup.tsx
+++ client/src/Signup.tsx
@@ -4,6 +4,7 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Container, Button } from '@material-ui/core';
 import type { Theme } from '@material-ui/core/styles';
+import { inherits } from 'util';
 
 const useStyles = makeStyles((theme: Theme) => ({
     imageBackground: {
@@ -15,25 +16,19 @@ const useStyles = makeStyles((theme: Theme) => ({
     },
     linkText: {
         color: theme.palette.text.primary,
-        textDecoration: 'none',
-        '& .MuiTypography-body1': {
-            fontSize: '1.6rem',
-        }
+        textDecoration: 'none'
     },
     formBox: {
-        width: '100%',
-        padding: '0 2rem 4rem 2rem',
-        marginTop: '3rem',
-        '& h2': {
-            width: '100%',
-        },
-    },
-    centerText: {
-        textAlign: 'center'
+        textAlign: 'center',
+        width: '100%'
     },
     positionOptions: {
         marginLeft: '20%',
-        marginTop: '5rem'
+        marginTop: '5rem',
+        alignContent: 'center'
+    },
+    caption: {
+        width: '200px'
     }
 }));
 
@@ -45,26 +40,22 @@ function Signup() {
         <Container className={classes.formBox}>
             <Typography variant="h2" component="h2">Welcome!</Typography>
             <Typography variant="h2" component="h2">Select your account type.</Typography>
-            <Typography variant="body1" component="caption">Already have an account? <Link to="/login">Log In</Link> </Typography>
+            <Typography variant="body2" component="caption">Already have an account? <Link to="/login">Log In</Link> </Typography>
 
             <Grid container spacing={8}>
                 <Grid item className={classes.positionOptions}>
-                    <div className={classes.centerText}>
-                        <div className={classes.imageBackground}></div>
-                        <Typography variant="body1" component="caption">Are you a non-profit organization?</Typography>
-                        <Link className={classes.linkText} to="/signup_nonprofit">
-                            <Button variant="contained">Create non-profit account</Button>
-                        </Link>
-                    </div>
+                    <div className={classes.imageBackground}></div>
+                    <Typography className={classes.caption} variant="body2" component="caption">Are you a non-profit organization?</Typography>
+                    <Link className={classes.linkText} to="/signup_nonprofit">
+                        <Button variant="contained">Create non-profit account</Button>
+                    </Link>
                 </Grid>
                 <Grid item className={classes.positionOptions}>
-                    <div className={classes.centerText}>
-                        <div className={classes.imageBackground}></div>
-                        <Typography variant="body1" component="caption">Are you an individual citizen?</Typography>
-                        <Link className={classes.linkText} to="/signup_citizen">
-                            <Button variant="contained">Create citizen account</Button>
-                        </Link>
-                    </div>
+                    <div className={classes.imageBackground}></div>
+                    <Typography className={classes.caption} variant="body2" component="caption">Are you an individual citizen?</Typography>
+                    <Link className={classes.linkText} to="/signup_citizen">
+                        <Button variant="contained">Create citizen account</Button>
+                    </Link>
                 </Grid>
             </Grid>
         </Container>
