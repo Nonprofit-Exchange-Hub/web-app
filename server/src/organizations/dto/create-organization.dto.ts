94089f4d24a1204ccc315376d357e74c85403883
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-11-21 14:19:12 -0500

    removes pseudocode

--- server/src/organizations/dto/create-organization.dto.ts
+++ server/src/organizations/dto/create-organization.dto.ts
@@ -1,9 +1,6 @@
 import { Organization } from '../entities/organization.entity'
 import { PartialType } from '@nestjs/mapped-types';
 
-// I could see Partial type being helpful in update, but why in create? I saw it in Users and Assets, but I don't see how some of the inputs here could be optional
-
-// also a question: do we want validators on any of these inputs using class-validator? 
 export class CreateOrganizationDto extends PartialType(Organization){
   name: string;
   description: string;
