76842659619c9f7d8b3b9dfa6e4d1136bb76cfa7
Zoe Friedman <ZOEBLAIR.FRIEDMAN@GMAIL.COM>
2021-11-29 12:27:11 -0500

    Create organizations resource (#74)
    * generates organization resource using crud generator
    
    * creates Organization entity
    
    * adds constructor to organizations service
    
    * adds return values for organizations.service and validators to organizations entity
    
    * removes pseudocode
    
    * removes console.logs
    
    * adds typing to return values in Service and Controller and adjusts uniqueness validation to solely the EIN
    
    * moves error handling to the controller, rather than the service
    
    * adjusts update function in controller
    
    Co-authored-by: James D. Rogers <jd2rogers2@gmail.com>

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,32 +1,35 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseConnectionService } from './database-connection.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
+import { DatabaseConnectionService } from './database-connection.service';
 import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { AssetsModule } from './assets/assets.module';
 import { MessagesModule } from './messages/messages.module';
 import { UsersModule } from './users/users.module';
+import { OrganizationsModule } from './organizations/organizations.module';
 
-import { join } from 'path';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-    }),
-    TypeOrmModule.forRootAsync({ useClass: DatabaseConnectionService }),
-    AssetsModule,
-    MessagesModule,
-    UsersModule,
-    AuthModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../..', 'client/build'),
-    }),
-  ],
-  controllers: [AppController],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
+        TypeOrmModule.forRootAsync({ useClass: DatabaseConnectionService }),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, '../..', 'client/build'),
+        }),
+        AssetsModule,
+        AuthModule,
+        MessagesModule,
+        OrganizationsModule,
+        UsersModule,
+
+    ],
+    controllers: [ AppController ],
+    providers: []
 })
 export class AppModule {}
---
+++ server/src/organizations/entities/organization.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('organizations')
+export class Organization {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text')
+  name: string;
+
+  @Column('text')
+  description: string;
+
+  @Column('text')
+  website: string;
+
+  @Column('text')
+  address: string;
+
+  @Column('text')
+  phone: string;
+
+  @Column('text')
+  city: string;
+
+  @Column('text')
+  state: string;
+
+  @Column({type:'int', unique: true})
+  ein: number;
+
+  @Column('int')
+  tax_exempt_id: number;
+}
