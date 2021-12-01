99105edd4c2d54412b08e0cc6be397157e00818d
Alexander Rovang <DXRovang@gmail.com>
2021-12-01 17:16:28 -0500

    adds basic columns to transaction.entity

---
+++ server/src/transactions/transaction.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+
+export class Transaction{
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  donater_user_id: string;
+
+  @Column()
+  donater_organization_id: string;
+
+  @Column()
+  requester_id: string;
+
+  @Column()
+  asset_id: string;
+
+  @Column()
+  status: string;
+
+  @Column()
+  created_date: string;
+}
