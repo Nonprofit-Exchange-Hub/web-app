bbadaff68f345a1f386364ad96d2a9d5fb16a173
Alexander Rovang <DXRovang@gmail.com>
2021-12-01 16:53:00 -0500

    imports TypeOrmModule and TransactionsRepository into transactions.module

--- server/src/transactions/transactions.module.ts
+++ server/src/transactions/transactions.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TransactionsController } from './transactions.controller';
 import { TransactionsService } from './transactions.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransactionsRepository } from './transactions.repository';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TransactionsRepository])
+  ]
   controllers: [TransactionsController],
   providers: [TransactionsService]
 })
