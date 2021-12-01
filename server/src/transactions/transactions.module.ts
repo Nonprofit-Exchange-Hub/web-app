d86d3c4385552d86624afaa013901e9e0ee07c3b
Alexander Rovang <DXRovang@gmail.com>
2021-12-01 16:48:52 -0500

    adds empty files/folders for transaction dto, create-transaction, controller, module, repository, and service

---
+++ server/src/transactions/transactions.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { TransactionsController } from './transactions.controller';
+import { TransactionsService } from './transactions.service';
+
+@Module({
+  controllers: [TransactionsController],
+  providers: [TransactionsService]
+})
+export class TransactionsModule {}
