88102866982170661eb7a5096a6726ca639c1ef4
James Rogers <jd2rogers2@gmail.com>
2021-12-04 08:26:31 -0800

    add eqeqeq rule, remove old commented eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,31 +1,8 @@
 module.exports = {
   extends: ['eslint:recommended', 'react-app'],
+  ignorePatterns: ['/build/**/*'],
   rules: {
     semi: ['error', 'always'],
-    // quotes: ['error', 'double'],
+    eqeqeq: ['error', 'always'],
   },
-  ignorePatterns: ['/build/**/*'],
-
-  // parser: '@typescript-eslint/parser',
-  // parserOptions: {
-  //   project: 'tsconfig.json',
-  //   sourceType: 'module',
-  // },
-  // plugins: ['@typescript-eslint/eslint-plugin'],
-  // extends: [
-  //   'plugin:@typescript-eslint/recommended',
-  //   'plugin:prettier/recommended',
-  // ],
-  // root: true,
-  // env: {
-  //   node: true,
-  //   jest: true,
-  // },
-  // ignorePatterns: ['.eslintrc.js'],
-  // rules: {
-  //   '@typescript-eslint/interface-name-prefix': 'off',
-  //   '@typescript-eslint/explicit-function-return-type': 'off',
-  //   '@typescript-eslint/explicit-module-boundary-types': 'off',
-  //   '@typescript-eslint/no-explicit-any': 'off',
-  // },
 };
