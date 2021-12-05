42acf1d26fbd728d84b1fa2683bee40ee3037ae4
Alexander Rovang <DXRovang@gmail.com>
2021-12-05 11:23:43 -0500

    adds GlobalPipes & ValidationPipe to main

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,13 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { resolve } from 'path';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.setGlobalPrefix('api');
     app.enableCors();
-
+    app.useGlobalPipes(new ValidationPipe())
     app.useStaticAssets(resolve('./src/public'));
     app.setBaseViewsDir(resolve('./src/views'));
     app.setViewEngine('hbs');
--- server/src/transactions/transactions.controller.ts
+++ server/src/transactions/transactions.controller.ts
@@ -10,17 +10,22 @@ export class TransactionsController {
   constructor(private transactionsService: TransactionsService) {}
 
   @Post()
-  createTransaction(@Body() createTransactionDto: CreateTransactionDto): Promise<Transaction> {
+  createTransaction(
+    @Body() 
+    createTransactionDto: CreateTransactionDto): Promise<Transaction> {
     return this.transactionsService.createTransaction(createTransactionDto);
   }
 
   @Get()
-  getTransactions(@Query() filterDto: GetTransactionsFilterDto): Promise<Transaction[]>{
+  getTransactions(
+    @Query() 
+    filterDto: GetTransactionsFilterDto): Promise<Transaction[]>{
     return this.transactionsService.getTransactions(filterDto)
   }
 
   @Get('/:id')
-  getTransactionById(@Param('id') id: string): Promise<Transaction>{
+  getTransactionById(
+    @Param('id') id: string): Promise<Transaction>{
     return this.transactionsService.getTransactionById(id)
   }
 
@@ -34,7 +39,8 @@ export class TransactionsController {
     }
 
   @Delete('/:id')
-  deleteTransaction(@Param('id') id: string): Promise<void>{
+  deleteTransaction(
+    @Param('id') id: string): Promise<void>{
     return this.transactionsService.deleteTransaction(id)
   }
 }
