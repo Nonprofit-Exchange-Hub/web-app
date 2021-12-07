b2de9994780fb6e0615a32e8865f1f2c9907057f
Alexander Rovang <DXRovang@gmail.com>
2021-12-07 15:48:27 -0500

    adds custom get-user decorator

---
+++ server/src/auth/get-user.decorator.ts
@@ -0,0 +1,7 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { User } from "src/users/entities/user.entity";
+
+export const GetUser = createParamDecorator((_data, ctx: ExecutionContext): User => {
+  const req = ctx.switchToHttp().getRequest()
+  return req.user
+})
