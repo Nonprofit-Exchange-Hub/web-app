4731b77301e30f9c43b7001cafa826f10d510721
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2021-12-07 16:55:42 -0800

    remove old comments

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -12,8 +12,6 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      //TODO I put a empty mock for UserService. For real testing we'll need
-      // to create a mock since we are using TypeORM
       providers: [UsersService],
     }).compile();
 
