1461d62aa0943df4e3937c99b0376acbc9f4985b
Ynda Jas <yndajas@gmail.com>
2021-12-21 21:44:45 +0000

    refactor: remove messages auth guards

--- server/src/messages/messages.controller.ts
+++ server/src/messages/messages.controller.ts
@@ -1,8 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { Message } from './entities/message.entity';
 import { DeleteResult } from 'typeorm';
 
@@ -10,23 +9,16 @@ import { DeleteResult } from 'typeorm';
 export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
-  // @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() createMessageDto: CreateMessageDto): Promise<Message> {
     return this.messagesService.create(createMessageDto);
   }
 
-  // TODO: reinstate UseGuards here and in the Delete action when
-  // ready - currently these are needed to add data to test the
-  // MessageInboxView component
-
-  // @UseGuards(JwtAuthGuard)
   @Get()
   async findAll(): Promise<Message[]> {
     return this.messagesService.findAll();
   }
 
-  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Message> {
     return this.messagesService.findOne(+id);
@@ -34,7 +26,6 @@ export class MessagesController {
 
   // add find by user and find by transaction
 
-  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   async update(
     @Param('id') id: string,
@@ -43,7 +34,6 @@ export class MessagesController {
     return this.messagesService.update(+id, updateMessageDto);
   }
 
-  // @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async remove(@Param('id') id: string): Promise<DeleteResult> {
     return this.messagesService.remove(+id);
