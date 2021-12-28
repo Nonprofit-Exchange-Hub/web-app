91a348f2b5d0e76fd6b4865dcd12eccbd1742002
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-12-28 14:29:26 -0500

    abandoning HTTPService

--- server/src/organizations/organizations.module.ts
+++ server/src/organizations/organizations.module.ts
@@ -3,19 +3,11 @@ import { OrganizationsService } from './organizations.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrganizationsController } from './organizations.controller';
 import { Organization } from './entities/organization.entity';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
   // registerAsync was recommended by docs: https://docs.nestjs.com/techniques/http-module
   imports: [
-    TypeOrmModule.forFeature([Organization]),
-    HttpModule.registerAsync({
-      useFactory: () => ({
-        timeout: 5000,
-        maxRedirects: 5,
-      }),
-    }),
-  ],
+    TypeOrmModule.forFeature([Organization])],
   controllers: [OrganizationsController],
   providers: [OrganizationsService],
 })
--- server/src/organizations/organizations.service.ts
+++ server/src/organizations/organizations.service.ts
@@ -4,16 +4,12 @@ import { UpdateOrganizationDto } from './dto/update-organization.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, DeleteResult } from 'typeorm';
 import { Organization } from './entities/organization.entity';
-import { HttpService } from '@nestjs/axios';
-import { AxiosResponse } from 'axios';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+
 
 @Injectable()
 export class OrganizationsService {
   constructor(
-    @InjectRepository(Organization) private organizationsRepository: Repository<Organization>,
-    private httpService: HttpService,
+    @InjectRepository(Organization) private organizationsRepository: Repository<Organization>
   ) {}
 
   async create(createOrganizationDto: CreateOrganizationDto): Promise<Organization> {
@@ -21,34 +17,23 @@ export class OrganizationsService {
 
     // verify name & ein before saving:
     const verified = await this.checkEIN(organization.name, organization.ein);
+    // if verified, return org, 
+    // if verified && 
+    // but should this logic be in my other function? i think so! 
     if (verified) {
       console.log('verified name & ein');
-      return this.organizationsRepository.save(organization);
+      // return this.organizationsRepository.save(organization);
+      return organization
     } else {
       console.log('cannot verify name & ein');
-      // handle error here
+      return organization
     }
   }
 
   // will compare name & ein to the propublica api database:
-  async checkEIN(name, ein): Promise<Boolean> {
+  async checkEIN(name, ein): Promise<boolean> {
     console.log('inside checkEIN func:', name, ein);
-
-    // i don't really know how to use map here, but i'm also wondering how to incorporate a json conversion if necessary...i tried to tag on .json() in the map call but it complicated things
-    const r = this.httpService
-      .get('https://projects.propublica.org/nonprofits/api/v2/search.json?q=propublica')
-      .pipe(map((res) => res));
-
-    // trying to see what the response data looks like:
-    console.log(r);
-
-    // or do I need to use subscribe? this didn't work either:
-    // await this.httpService.get("https://projects.propublica.org/nonprofits/api/v2/search.json?q=propublica").pipe(map(data => {})).subscribe(result => {
-    //   console.log(result);
-    // });
-
-    // LOGIC WILL GO HERE TO COMPARE
-    // if they are the same return true. else return false. hardcoded to return false for now:
+    // put fetch here
     return false;
   }
 
