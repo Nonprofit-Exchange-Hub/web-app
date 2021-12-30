b96c1d4d8e3f9c39fd8938604b72907dc6855862
James Rogers <jd2rogers2@gmail.com>
2021-12-30 09:35:17 -0800

    use express request and response types

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post, Response, Request, UseGuards, Get } from '@nestjs/common';
 
+import type { Request as RequestT, Response as ResponseT } from 'express';
+
 import { LoginAuthGuard } from './guards/login-auth.guard';
 import { CookieAuthGuard } from './guards/cookie-auth.guard';
 import { AuthService } from './auth.service';
@@ -7,13 +9,18 @@ import { COOKIE_KEY } from './constants';
 
 import type { User } from '../users/entities/user.entity';
 
+type AuthedRequest = RequestT & { user: User };
+
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
   @UseGuards(LoginAuthGuard)
-  async login(@Request() request, @Response({ passthrough: true }) response): Promise<void> {
+  async login(
+    @Request() request: AuthedRequest,
+    @Response({ passthrough: true }) response: ResponseT,
+  ): Promise<void> {
     const { user } = request;
     const jwt = await this.authService.createJwt(user);
     response
@@ -30,13 +37,13 @@ export class AuthController {
 
   @Post('session')
   @UseGuards(CookieAuthGuard)
-  async session(@Request() request): Promise<{ user: Omit<User, 'password'> }> {
+  async session(@Request() request: AuthedRequest): Promise<{ user: Omit<User, 'password'> }> {
     const { user } = request;
     return { user };
   }
 
   @Get('logout')
-  logout(@Response({ passthrough: true }) response): void {
+  logout(@Response({ passthrough: true }) response: ResponseT): void {
     response.clearCookie(COOKIE_KEY).send();
   }
 }
