8ac0ab22de8d387b1a891dc43db75e801d245898
Kevin Hines <kevhines@gmail.com>
2022-02-02 14:39:25 -0500

    minor change to users controller in comments

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Request, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Request,
+  ParseIntPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -19,26 +29,25 @@ export class UsersController {
 
   @Post('reset_password')
   async resetPassword(@Request() req) {
-      //console.log(req.body.email)
-      let user;
-      try {
-        user = await this.usersService.findByEmail(req.body.email);
-      } catch(e) {
-        //console.error(e);
-      }
-      if (user) {
-       // console.log("got a user")
-        //console.log(user)
-        //great send email if you get here 
-        //that email has a link to take you to update your password
-      } else {
-       // console.log("No User")
-       //do nothing
-      }
-      //reading online it looks like I might need to  make a table for reset-password. 
-      //delete entries after password is reset OR if user is not found?
-      //this is basically to create and keep track of a token (that will expire)
-      //I need to read more
+    //console.log(req.body.email)
+    let user;
+    try {
+      user = await this.usersService.findByEmail(req.body.email);
+    } catch (e) {
+      //console.error(e);
+    }
+    if (user) {
+      // console.log("got a user")
+      //console.log(user)
+      //great send email if you get here
+      //that email has a link to take you to update your password
+    } else {
+      // console.log("No User")
+      //do nothing
+    }
+    //reading online it looks like I might need to  make a table for reset-password.
+    //delete entries after password is reset OR if user is not found?
+    //this is basically to create and keep track of a token (that will expire)
   }
 
   @Get(':id')
