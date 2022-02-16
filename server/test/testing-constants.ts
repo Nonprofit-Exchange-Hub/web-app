29281da9629974bcbaa1762363c8d0325cb83b4d
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2022-02-15 16:58:41 -0800

    one .env

--- server/test/testing-constants.ts
+++ server/test/testing-constants.ts
@@ -4,15 +4,15 @@
 
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
-dotenv.config({ path: __dirname + '/e2e.env' });
+dotenv.config({ path: __dirname + '/../.env' });
 
 export const TEST_DB_OPTIONS: TypeOrmModuleOptions = {
   name: 'default',
   type: 'postgres',
-  host: process.env.E2E_DATABASE_HOST,
-  port: Number(process.env.E2E_DATABASE_PORT),
-  username: process.env.E2E_DATABASE_USER,
-  password: process.env.E2E_DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
   database: process.env.E2E_DATABASE_DB,
   synchronize: true, //shouldn't be used in production
   dropSchema: false, //toggle to true to clear database schema
