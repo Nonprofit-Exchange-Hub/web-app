4b4a3fe0a741f710d33ae5dfd47d9430ca1427d0
Pamela Torres-Rocca <Pamela@Torres-Rocca.com>
2022-04-28 15:20:10 -0400

    seeder.service.ts add async seeding methods

--- server/src/seeder/seeder.service.ts
+++ server/src/seeder/seeder.service.ts
@@ -1,12 +1,35 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { seedUsers } from '../database/seeding/seed-data';
+import { seedAssets } from '../database/seeding/seed-data';
+import { seedCategories } from '../database/seeding/seed-data';
+import { seedMessages } from '../database/seeding/seed-data';
+import { seedOrganizations } from '../database/seeding/seed-data';
+import { seedTransactions } from '../database/seeding/seed-data';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { CreateAssetDto } from '../assets/dto/create-asset.dto';
+import { CreateCategoryDto } from '../categories/dto/create-category.dto';
+import { CreateOrganizationDto } from '../organizations/dto/create-organization.dto';
+import { CreateMessageDto } from '../messages/dto/create-message.dto';
+import { CreateTransactionDto } from '../transactions/dto/create-transaction.dto';
 import { UsersService } from '../users/users.service';
+import { AssetsService } from '../assets/assets.service';
+import { CategoriesService } from '../categories/categories.service';
+import { MessagesService } from '../messages/messages.service';
+import { OrganizationsService } from '../organizations/organizations.service';
+import { TransactionsService } from '../transactions/transactions.service';
 import { Connection, EntityMetadata, Repository } from 'typeorm';
 
 @Injectable()
 export class SeederService {
-  constructor(private readonly userService: UsersService, private readonly connx: Connection) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly assetService: AssetsService,
+    private readonly categoryService: CategoriesService,
+    private readonly organizationService: OrganizationsService,
+    private readonly messageService: MessagesService,
+    private readonly transactionService: TransactionsService,
+    private readonly connx: Connection,
+  ) {}
 
   /**
    * Seeds demo users if they don't exist
@@ -18,6 +41,41 @@ export class SeederService {
         Logger.log('Seeding Users', SeederService.name);
         this.userService.create(user).catch((err) => Logger.log(err));
       }
+    });
+  }
+
+  public async seedAssetsAsync(): Promise<void> {
+    seedAssets().forEach(async (asset: CreateAssetDto) => {
+      Logger.log('Seeding Assets', SeederService.name);
+      this.assetService.create(asset).catch((err) => Logger.log(err));
+    });
+  }
+
+  public async seedCategoriesAsync(): Promise<void> {
+    seedCategories().forEach(async (category: CreateCategoryDto) => {
+      Logger.log('Seeding Categories', SeederService.name);
+      this.categoryService.create(category).catch((err) => Logger.log(err));
+    });
+  }
+
+  public async seedOrganizationsAsync(): Promise<void> {
+    seedOrganizations().forEach(async (organization: CreateOrganizationDto) => {
+      Logger.log('Seeding Organizations', SeederService.name);
+      this.organizationService.create(organization).catch((err) => Logger.log(err));
+    });
+  }
+
+  public async seedMessagesAsync(): Promise<void> {
+    seedMessages().forEach(async (message: CreateMessageDto) => {
+      Logger.log('Seeding Messages', SeederService.name);
+      this.messageService.create(message).catch((err) => Logger.log(err));
+    });
+  }
+
+  public async seedTransactionsAsync(): Promise<void> {
+    seedTransactions().forEach(async (transaction: CreateTransactionDto) => {
+      Logger.log('Seeding Transactions', SeederService.name);
+      this.transactionService.createTransaction(transaction).catch((err) => Logger.log(err));
     });
   }
 
