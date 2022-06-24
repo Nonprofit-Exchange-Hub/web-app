facae779fc18b1a5569be34d52f5a39f4d4c85a4
James Rogers <jd2rogers2@gmail.com>
2022-06-24 08:34:17 -0700

    add lerna, move client and server in packages, add lerna commands, fix husky commands, fix husky workflows

--- packages/client/src/features/static/policy/TrustAndSafety.tsx
+++ packages/client/src/features/static/policy/TrustAndSafety.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import makeStyles from '@mui/styles/makeStyles';
-import { Box, Grid, Container } from '@mui/material/';
+import { Box, Grid, Container } from '@mui/material';
 
 import type { Theme } from '@mui/material/styles';
 
