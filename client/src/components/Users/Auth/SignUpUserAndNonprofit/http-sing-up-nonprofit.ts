5b0e0de9a19514dd8d5ab81c5c7bbe2779e20b7b
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2022-10-25 17:57:52 -0700

    use dynamic url from config

--- client/src/components/Users/Auth/SignUpUserAndNonprofit/http-sing-up-nonprofit.ts
+++ client/src/components/Users/Auth/SignUpUserAndNonprofit/http-sing-up-nonprofit.ts
@@ -1,15 +1,19 @@
 import axios, { AxiosResponse } from 'axios';
 import { toOrg, toUser } from './helpers';
 import { FormData } from './FormData';
+import { APP_API_BASE_URL } from '../../../../configs';
 
 export const httpPostNonprofitSignup = (data: FormData): Promise<AxiosResponse<any, any>> => {
   const user = toUser(data);
   const organization = toOrg(data);
-  const api = new URL(`http://localhost:3001/api/userOrganizations`);
+  // eslint-disable-next-line no-undef
+  const USER_ORGS_API_URL = `${APP_API_BASE_URL}/userOrganizations`;
+  const api = new URL(USER_ORGS_API_URL);
   return axios.post(api.href, { organization, user });
 };
 
 export const httpGetValidateEin = (ein: string): Promise<AxiosResponse<any, any>> => {
-  const url = new URL(`http://localhost:3001/api/organizations/ein/${ein}`);
+  const ORGS_API_URL = `${APP_API_BASE_URL}/organizations`;
+  const url = new URL(`${ORGS_API_URL}/ein/${ein}`);
   return axios.get(url.href);
 };
