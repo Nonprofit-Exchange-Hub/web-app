33b1f6273ba1629975547b181aae4799967c56f2
Alex <92830192+EtoKruto@users.noreply.github.com>
2022-11-28 17:45:29 -0800

    Nonprofit search (#230)
    * initial commit on branch: -change route name to SearchResults -passed state to SearchResults from Search component -add popover hint onClick search
    
    * -update Search Result Component; -Removed link when SearchIcon; -Added dropdown; -confirm fetch API working
    
    * -corrections made per comments; -VolunteerList deleted for now until we confirm the database/type
    
    * add 'history.push' to useEffect to synchornize URLparams with search results
    
    * fix small state bugs
    
    * remove comments; reduce code
    
    * Update SearchResults.tsx
    
    * updated latest comments to include query as the truth for all handlers and fetches
    
    * use query search text and category in render, handlers, useEffect. this way search input is independent of page results and only updates results on submit
    
    Co-authored-by: jd2rogers2 <jd2rogers2@gmail.com>

---
+++ client/src/views/OrgsList.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+import Typography from '@mui/material/Typography';
+import makeStyles from '@mui/styles/makeStyles';
+import Card from '@mui/material/Card';
+import InfoIcon from '@mui/icons-material/Info';
+import RoomOutlined from '@mui/icons-material/RoomOutlined';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+
+import type { Theme } from '@mui/material/styles';
+
+import type { Organization } from '../types';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  orgsHeaderSub: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+    alignItems: 'stretch',
+  },
+  card: {
+    marginBottom: '30px',
+    '&:not(:last-child)': {
+      marginRight: '20px',
+    },
+    flex: '0 0 30%',
+  },
+  cardImg: {
+    borderRadius: '5px',
+    margin: '10%',
+    maxWidth: '80%',
+  },
+  orgsHeader: {
+    textAlign: 'left',
+    paddingBottom: '20px',
+  },
+  cardText1: {
+    padding: '0 10%',
+  },
+  cardText2: {
+    padding: '0 10% 10%',
+  },
+}));
+
+type Props = {
+  orgs: Organization[];
+  headerContentRight?: JSX.Element;
+  headerText: string;
+};
+
+function OrgsList(props: Props): JSX.Element {
+  const classes = useStyles();
+  const { orgs, headerContentRight, headerText } = props;
+  if (!orgs) return <> </>;
+
+  return (
+    <>
+      <Grid container item justifyContent="space-between">
+        <Typography variant="h4" component="h4" className={classes.orgsHeader}>
+          {headerText}
+        </Typography>
+        {headerContentRight ?? null}
+      </Grid>
+      <div className={classes.orgsHeaderSub}>
+        {orgs.map((org) => (
+          <NavLink to={`/to={/organizations/${org.id}`} key={org.id} className={classes.card}>
+            <Card variant="outlined">
+              <Box sx={{ display: 'flex' }}>
+                <Typography variant="h6" component="h4" className={classes.cardText1}>
+                  {org.name}
+                </Typography>
+                <div className={classes.cardText2}>
+                  <InfoIcon />
+                  {org.description}
+                </div>
+                <div className={classes.cardText2}>
+                  <RoomOutlined />
+                  {org.address}
+                  <br />
+                </div>
+              </Box>
+            </Card>
+          </NavLink>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default OrgsList;
---
+++ client/src/views/SearchResults.tsx
@@ -0,0 +1,272 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import * as React from 'react';
+import * as queryString from 'query-string';
+import { NavLink, useHistory } from 'react-router-dom';
+
+import makeStyles from '@mui/styles/makeStyles';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import Tooltip from '@mui/material/Tooltip';
+
+import type { Theme } from '@mui/material/styles';
+
+import AssetsList from './AssetsList';
+import OrgsList from './OrgsList';
+import FilterGroup from '../components/FilterGroup';
+import { filters1, filters2, filters3 } from '../assets/temp';
+import { APP_API_BASE_URL } from '../configs';
+
+import type { Asset, Organization } from '../types';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  searchBar: {
+    backgroundColor: 'white',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: '10px 5%',
+    borderRadius: 0,
+    alignItems: 'center',
+  },
+  iconButton: {
+    padding: 10,
+    '&:hover': {
+      backgroundColor: 'inherit',
+      borderRadius: '10px',
+    },
+  },
+  searchInput: {
+    display: 'flex',
+    alignItems: 'center',
+    paddingLeft: '10px',
+  },
+  contentWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  },
+  leftPanel: {
+    borderRight: '1px solid grey',
+    width: '20%',
+  },
+  createBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  rightPanel: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    padding: '10px 5%',
+    width: '80%',
+  },
+  makeAPost: {
+    fontWeight: 'bold',
+    textDecoration: 'none',
+    color: 'black',
+  },
+}));
+
+function SearchResults(): JSX.Element {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const { search: querySearchText, category: querySearchCategory } = queryString.parse(
+    history.location.search,
+  );
+
+  const [searchCategory, setSearchCategory] = React.useState<string>(
+    (querySearchCategory as string) || '',
+  );
+  const [searchText, setSearchText] = React.useState<string>((querySearchText as string) || '');
+  const [selectedFilters, setSelectedFilters] = React.useState<{ [key: string]: boolean }>({});
+
+  const [offers, setOffers] = React.useState<Asset[]>([]);
+  const [needs, setNeeds] = React.useState<Asset[]>([]);
+  const [orgs, setOrgs] = React.useState<Organization[]>([]);
+  //TODO: create Volunteer Type in types/index.ts and change Object[] to Volunteer[] below
+  const [volunteer, setVolunteer] = React.useState<Object[]>([]);
+
+  function fetchSearchData() {
+    if (querySearchCategory === 'Volunteer') {
+      //TODO: Change API to /volunteer
+      fetch(`${APP_API_BASE_URL}/organizations`)
+        .then((resp) => resp.json())
+        .then((data: Asset[]) => {
+          setVolunteer(data);
+        });
+    } else if (querySearchCategory === 'Offers' || querySearchCategory === 'Needs') {
+      fetch(
+        `${APP_API_BASE_URL}/assets?type=${
+          querySearchCategory === 'Needs' ? 'donation' : 'request'
+        }${querySearchText ? `&title=${querySearchText}` : ''}`,
+      )
+        .then((resp) => resp.json())
+        .then((data: Asset[]) => {
+          if (querySearchCategory === 'Needs') {
+            setNeeds(data);
+          } else {
+            setOffers(data);
+          }
+        });
+    } else if (querySearchCategory === 'Nonprofits') {
+      fetch(`${APP_API_BASE_URL}/organizations?search=${querySearchText}`)
+        .then((resp) => resp.json())
+        .then((data: Organization[]) => {
+          setOrgs(data);
+        });
+    } else {
+      //TODO: Change API to fetch ALL combined data
+    }
+  }
+
+  const handleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedFilters({
+      ...selectedFilters,
+      [event.target.value]: event.target.checked,
+    });
+  };
+
+  const handleSearch = () => {
+    history.push(`/SearchResults?search=${searchText}&category=${searchCategory}`);
+  };
+
+  React.useEffect(() => {
+    fetchSearchData();
+  }, [querySearchText, querySearchCategory]);
+
+  return (
+    <>
+      <Paper className={classes.searchBar}>
+        <Box sx={{ display: 'flex' }}>
+          <>
+            X of Y results for "<b>{searchText}</b>"
+          </>
+        </Box>
+        <Box sx={{ display: 'flex' }}>
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={searchCategory}
+              label="Age"
+              onChange={(e: SelectChangeEvent) => {
+                setSearchCategory(e.target.value);
+              }}
+            >
+              <MenuItem value="All">All</MenuItem>
+              <MenuItem value="Nonprofits">Nonprofits</MenuItem>
+              <MenuItem value="Needs">Needs</MenuItem>
+              <MenuItem value="Offers">Offers</MenuItem>
+              <MenuItem value="Volunteer">Volunteer</MenuItem>
+            </Select>
+          </FormControl>
+
+          <Paper className={classes.searchInput}>
+            <Tooltip
+              placement="top-end"
+              componentsProps={{
+                tooltip: {
+                  sx: {
+                    color: 'rgba(0, 0, 0, 0.87)',
+                    fontSize: 13,
+                    bgcolor: 'common.white',
+                    '& .MuiTooltip-arrow': {
+                      color: 'common.white',
+                    },
+                  },
+                },
+              }}
+              title="Press 'Enter' key or Click the Icon to Search"
+            >
+              <TextField
+                sx={{
+                  '& .MuiOutlinedInput-notchedOutline': {
+                    border: '0 none',
+                  },
+                }}
+                placeholder="Search"
+                inputProps={{ 'aria-label': 'ex. diapers' }}
+                type="text"
+                value={searchText}
+                onKeyDown={(e) => e.key === 'Enter' && handleSearch()}
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
+                  setSearchText(e.target.value);
+                }}
+              />
+            </Tooltip>
+            <SearchIcon fontSize="large" onClick={handleSearch} />
+          </Paper>
+        </Box>
+      </Paper>
+      <div className={classes.contentWrapper}>
+        <div className={classes.leftPanel}>
+          <FilterGroup
+            header="Location"
+            onHandleCheck={handleCheck}
+            filters={filters1}
+            selectedFilters={selectedFilters}
+          />
+          <FilterGroup
+            header="Category"
+            onHandleCheck={handleCheck}
+            filters={filters2}
+            selectedFilters={selectedFilters}
+          />
+          <FilterGroup
+            header="Nonprofit"
+            onHandleCheck={handleCheck}
+            filters={filters3}
+            selectedFilters={selectedFilters}
+          />
+        </div>
+        <div className={classes.rightPanel}>
+          <Paper elevation={0} className={classes.createBar}>
+            {/* TODO: where is this meant to link to? */}
+            <NavLink className={classes.makeAPost} to="/create">
+              <Button color="primary" variant="contained">
+                Make a Post
+              </Button>
+            </NavLink>
+          </Paper>
+          {/* {TODO:   Need to combine all lists under one "List" component} */}
+          {/* {TODO:   Create and add a VolunteerList component below */}
+          {querySearchCategory === 'All' ? (
+            <div>
+              {needs.length > 0 ? <AssetsList headerText={'Needs'} assets={needs} /> : <></>}
+              {offers.length > 0 ? <AssetsList headerText={'Offers'} assets={offers} /> : <></>}
+              {orgs.length > 0 ? <OrgsList headerText={'Nonprofits'} orgs={orgs} /> : <></>}
+              {volunteer.length > 0 ? `<VolunteerList Componet TBD />` : <></>}
+            </div>
+          ) : (
+            <></>
+          )}
+          {querySearchCategory === 'Needs' || querySearchCategory === 'Offers' ? (
+            <AssetsList
+              headerText={querySearchCategory === 'Needs' ? 'Needs' : 'Offers'}
+              assets={querySearchCategory === 'Needs' ? needs : offers}
+            />
+          ) : (
+            <></>
+          )}
+          {querySearchCategory === 'Nonprofits' ? (
+            <OrgsList headerText={'Nonprofits'} orgs={orgs} />
+          ) : (
+            <></>
+          )}
+          {querySearchCategory === 'Volunteer' ? `<VolunteerList Componet TBD />` : <></>}
+          {needs.length + offers.length + orgs.length + volunteer.length === 0 && 'No Results'}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default SearchResults;
