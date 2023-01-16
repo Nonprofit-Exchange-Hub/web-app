dba4cf89704b1bcc6ad7787b62edcf6a7efdf342
Brody Losh <48573405+brodylosh@users.noreply.github.com>
2023-01-16 12:20:20 -0500

    Moved Doing Business As Field To First Tab (#262)

--- client/src/components/Users/Auth/SignUpUserAndNonprofit/SignUpUserAndNonprofit.tsx
+++ client/src/components/Users/Auth/SignUpUserAndNonprofit/SignUpUserAndNonprofit.tsx
@@ -278,6 +278,27 @@ export const SignUpUserAndNonprofit = () => {
                   </Grid>
                   <Grid item md={12} xs={12}>
                     <Controller
+                      name="doing_business_as"
+                      control={control}
+                      defaultValue={''}
+                      render={({ field }) => (
+                        <TextField
+                          {...field}
+                          fullWidth
+                          label="Organization Name"
+                          placeholder="Organization"
+                          helperText={
+                            errors.doing_business_as?.message
+                              ? errors.doing_business_as.message
+                              : ''
+                          }
+                          error={!!errors.doing_business_as}
+                        />
+                      )}
+                    />
+                  </Grid>
+                  <Grid item md={12} xs={12}>
+                    <Controller
                       name="image_url"
                       control={control}
                       defaultValue={'https://blah.png'}
@@ -298,27 +319,6 @@ export const SignUpUserAndNonprofit = () => {
 
               {activeStep === 1 && (
                 <Grid container spacing={5}>
-                  <Grid item md={12} xs={12}>
-                    <Controller
-                      name="doing_business_as"
-                      control={control}
-                      defaultValue={''}
-                      render={({ field }) => (
-                        <TextField
-                          {...field}
-                          fullWidth
-                          label="Organization Name"
-                          placeholder="Organization"
-                          helperText={
-                            errors.doing_business_as?.message
-                              ? errors.doing_business_as.message
-                              : ''
-                          }
-                          error={!!errors.doing_business_as}
-                        />
-                      )}
-                    />
-                  </Grid>
                   <Grid item md={12} xs={12}>
                     <Controller
                       name="description"
