2554d0068749e85090e43df8a2ba5745c6e1ff48
humorous-storage-7521 <ryanwerner50@gmail.com>
2023-02-05 16:07:44 -0500

    resolve conflicts

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,4 +1,3 @@
-import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import makeStyles from '@mui/styles/makeStyles';
 
@@ -7,68 +6,6 @@ import QuestionList from '../components/QuestionList';
 import BannerSection from '../components/BannerSection';
 import NeedsCarousel from '../components/NeedsCarousel';
 import DonationsCarousel from '../components/DonationsCarousel';
-import routes from '../routes/routes';
-import CallToActionCards from '../components/CallToActionCards';
-import { APP_API_BASE_URL } from '../configs';
-
-import type { Asset } from '../types';
-
-const carouselSteps = [
-  [
-    {
-      label: 'cat 1',
-      imgPath: 'http://placekitten.com/150/150',
-    },
-    {
-      label: 'cat 1',
-      imgPath: 'http://placekitten.com/151/151',
-    },
-    {
-      label: 'cat 1',
-      imgPath: 'http://placekitten.com/152/152',
-    },
-    {
-      label: 'cat 1',
-      imgPath: 'http://placekitten.com/153/153',
-    },
-  ],
-  [
-    {
-      label: 'cat 2',
-      imgPath: 'http://placekitten.com/154/154',
-    },
-    {
-      label: 'cat 2',
-      imgPath: 'http://placekitten.com/155/155',
-    },
-    {
-      label: 'cat 2',
-      imgPath: 'http://placekitten.com/156/156',
-    },
-    {
-      label: 'cat 2',
-      imgPath: 'http://placekitten.com/157/157',
-    },
-  ],
-  [
-    {
-      label: 'cat 3',
-      imgPath: 'http://placekitten.com/158/158',
-    },
-    {
-      label: 'cat 3',
-      imgPath: 'http://placekitten.com/159/159',
-    },
-    {
-      label: 'cat 3',
-      imgPath: 'http://placekitten.com/160/160',
-    },
-    {
-      label: 'cat 3',
-      imgPath: 'http://placekitten.com/161/161',
-    },
-  ],
-];
 
 const loremIpsum =
   'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis placerat et, at vel tristique. Ac, gravida in quam gravida. Vel pretium nunc cursus donec enim. Sapien facilisis mauris justo, augue pharetra. Dignissim euismod fermentum sit gravida ut.';
