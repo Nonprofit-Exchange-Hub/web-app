32a49b4a6ef0edb905f8153c3dade9e74d3d3c84
Brody Losh <48573405+brodylosh@users.noreply.github.com>
2023-02-25 14:10:32 -0500

    Faqs Section (#282)
    * Backend Properties Added For Email Opt Out - 189
    
    * Email Opt-Out Frontend Input On Form
    
    * Frontend and Backend Updates 189
    
    * Updated Seed Data With New Property
    
    * Added Checkbox To NonProfit SignUp Form and Opt-Out Property Naming
    
    * Updated StubData to pass tests
    
    * Updated UsersControllere2eSpec
    
    * Updated Users.Stub.ts
    
    * Updated FAQs Styling
    
    * Deleted Users Folder
    
    * Updated README.md
    
    * Deleted server/src/auth directory
    
    * Updated package-lock.json
    
    * Updated FAQs Image Styling
    
    * Resolved package-lock issue and updated styling

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,30 +1,14 @@
-import Typography from '@mui/material/Typography';
 import makeStyles from '@mui/styles/makeStyles';
 
 import Search from '../components/Search';
-import QuestionList from '../components/QuestionList';
 import BannerSection from '../components/BannerSection';
 import NeedsCarousel from '../components/NeedsCarousel';
 import DonationsCarousel from '../components/DonationsCarousel';
-
-const loremIpsum =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Facilisis placerat et, at vel tristique. Ac, gravida in quam gravida. Vel pretium nunc cursus donec enim. Sapien facilisis mauris justo, augue pharetra. Dignissim euismod fermentum sit gravida ut.';
-
-const faqQuestions = [
-  { question: 'How does it work?', answer: loremIpsum.slice(0, 150) },
-  { question: 'Who can use the platform?', answer: loremIpsum.slice(0, 150) },
-  { question: 'What services can I offer?', answer: loremIpsum.slice(0, 150) },
-];
+import FAQs from '../components/FAQs';
 
 const useStyles = makeStyles(() => ({
   needsAndOffers: {
     padding: '10%',
-  },
-  faqs: {
-    padding: '10%',
-  },
-  faqsHeader: {
-    paddingBottom: '30px',
   },
   makeAPostButton: {
     marginLeft: '15px',
@@ -50,12 +34,7 @@ function Home(): JSX.Element {
       <BannerSection />
       <NeedsCarousel label={'Recent Needs From Nonprofits'} />
       <DonationsCarousel label={'Recent Offers From Users'} />
-      <div className={classes.faqs}>
-        <Typography variant="h4" component="h4" className={classes.faqsHeader}>
-          FAQs
-        </Typography>
-        <QuestionList questionList={faqQuestions}></QuestionList>
-      </div>
+      <FAQs />
     </>
   );
 }
