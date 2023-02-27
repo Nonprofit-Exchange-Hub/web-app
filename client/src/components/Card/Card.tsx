734cbc2cd81be35161926f37807512f5799bdd9e
etokruto <akrut26@gmail.com>
2023-02-26 19:38:38 -0800

    complete style card based on figma design; introduce reusable component ProgressBar

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -17,19 +17,26 @@ type Props = {
 };
 
 export default function Card(props: Props) {
+  const boxType = {
+    padding: '0.5em 8% 5% 8%',
+    display: 'flex',
+    background: (() => {
+      switch (props.type) {
+        case 'need':
+          return `${theme.palette.primary.gradient}`;
+        case 'donation':
+          return `${theme.palette.secondary.gradient}`;
+        default:
+          return `${theme.palette.background.default}`;
+      }
+    })(),
+    // add other styles as needed
+  };
   return (
     <MuiCard sx={{ width: '20em' }}>
       <CardActionArea>
         {props.children}
-        <Box
-          sx={{
-            padding: '0.5em 8% 5% 8%',
-            background:
-              props.type === 'need'
-                ? `${theme.palette.primary.gradient}`
-                : `${theme.palette.secondary.gradient}`,
-          }}
-        >
+        <Box style={{ padding: '.2em 4% 1% 4%', display: 'flex' }}>
           <Avatar
             sx={{
               display: 'inline-flex',
@@ -42,26 +49,26 @@ export default function Card(props: Props) {
             src={props.avatarUrl || ''}
             alt={props.org}
           >
-            {props.org?.slice(0, 1)}
+            {props.avatarUrl ? props.avatarUrl : props.org?.slice(0, 1)}
           </Avatar>
           <MuiBox
             style={{ display: 'inline-flex', flexDirection: 'column', justifyContent: 'center' }}
           >
             <Typography
               variant="body2"
-              color={theme.palette.secondary.contrastText}
               sx={{ fontSize: '0.9em !important', fontWeight: '300', marginBottom: '0 !important' }}
             >
-              {`Posted on ${props.datePosted ? props.datePosted.toLocaleDateString() : ''}`}
+              {`Posted by `}
             </Typography>
-            <Typography
-              variant="body2"
-              color={theme.palette.secondary.contrastText}
-              sx={{ marginTop: '0 !important' }}
-            >
+            <Typography style={{ fontSize: '1.1em' }} sx={{ marginTop: '0 !important' }}>
               by {`${props.org}`}
             </Typography>
           </MuiBox>
+        </Box>
+        <Box sx={boxType} style={{ padding: '0.5em 8% 5% 8%', display: 'flex', height: '2em' }}>
+          <Typography variant="h6" color={theme.palette.secondary.contrastText}>
+            {props.type.charAt(0).toUpperCase() + props.type.slice(1)}
+          </Typography>
         </Box>
       </CardActionArea>
     </MuiCard>
--- client/src/components/Card/NeedCard.tsx
+++ client/src/components/Card/NeedCard.tsx
@@ -4,9 +4,12 @@ import Card from './Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import { Tooltip } from '@mui/material';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import theme from '../../theme';
 import Example from '../../assets/need-example.png';
+import ProgressBar from './ProgressBar';
+import GivingfulG from './../../assets/GivingfulG.svg';
 
 type Props = {
   title: string;
@@ -21,46 +24,66 @@ type Props = {
   imgUrls?: string[];
   children?: ReactNode | ReactNode[];
 };
+const ellipsesStyle = {
+  whiteSpace: 'nowrap',
+  textOverflow: 'ellipsis',
+  width: '100%',
+  boxSizing: 'border-box',
+  overflow: 'hidden',
+};
 
 export default function NeedCard(props: Props) {
+  console.log(props);
   const image = props.imgUrls?.length ? props.imgUrls[0] : Example;
   return (
-    <Card title={props.title} type={props.type} datePosted={props.datePosted} org={props.org}>
+    <Card
+      title={props.title}
+      type={props.type}
+      datePosted={props.datePosted}
+      org={props.org}
+      sx={props.sx || {}}
+    >
       <CardContent sx={{ margin: '5% 5% 0% 5%' }}>
-        <Box sx={{ display: 'flex', justifyContent: 'space-between ' }}>
-          <Typography gutterBottom>Apparel</Typography>
-          <Typography gutterBottom sx={{ fontWeight: 900 }}>
-            Long Term
-          </Typography>
-        </Box>
-        <Typography variant="h1">Blazers</Typography>
-        <Box sx={{ marginTop: theme.spacing(1) }}>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            {props.location ? (
-              <>
-                <LocationOnIcon />
-                <Typography sx={{ marginLeft: '0.5em' }} gutterBottom>
-                  {props.location}
-                </Typography>
-              </>
-            ) : null}
+        <Box sx={{ padding: '0.5em' }}>
+          <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '3%' }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', marginBottom: '5%' }}>
+              <Typography style={{ marginBottom: '.4em' }}> {'{x} of {y} needed'}</Typography>
+              <ProgressBar progress={60}></ProgressBar>
+            </Box>
+            <Typography gutterBottom style={{ fontWeight: 900 }}>
+              {'{urgency}'}
+            </Typography>
           </Box>
+          <Typography gutterBottom>{'{category}'}</Typography>
+          <Tooltip title={props.title}>
+            <Typography variant="h1" sx={ellipsesStyle}>
+              {props.title}
+            </Typography>
+          </Tooltip>
+        </Box>
+        <Box sx={{ marginTop: theme.spacing(1) }}>
           <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: '-0.5em' }}>
             <LocationOnIcon />
-            <Typography gutterBottom>Hurricane Relief Effort</Typography>
+            <Typography gutterBottom>
+              {props.location ? props.location : 'Location Not Available'}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', marginLeft: '-0.5em' }}>
+            <img src={GivingfulG} alt="My SVG" style={{ width: '27px' }} />
+            <Typography gutterBottom>{'{effort}'}</Typography>
           </Box>
           <Box sx={{ marginTop: theme.spacing(1) }}>
             <Typography
               variant="body2"
               color={theme.palette.text.secondary}
-              style={{ height: '3em', overflow: 'hidden', marginLeft: '0.5em' }}
+              style={{ height: '3em', overflow: 'hidden' }}
             >
               {props.description}
             </Typography>
           </Box>
         </Box>
       </CardContent>
-      <CardMedia component="img" image={image} alt="Need example" sx={{ height: '90px' }} />
+      <CardMedia component="img" image={image} alt="Need example" sx={{ height: '6em' }} />
     </Card>
   );
 }
