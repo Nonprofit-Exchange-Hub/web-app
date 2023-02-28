8c435300dec0c1599381964796f6ee2bc5913a69
jd2rogers2 <jd2rogers2@gmail.com>
2023-02-27 17:31:52 -0800

    swagger like mick jagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,13 +3,16 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 dotenv.config({ path: __dirname + '/.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.setGlobalPrefix('/api');
   app.enableCors({
     credentials: true,
@@ -18,6 +21,16 @@ async function bootstrap() {
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser('secret_placeholder'));
+
+  const config = new DocumentBuilder()
+    .setTitle('Nonprofit Circle API')
+    .setDescription('Our API monolith')
+    .setVersion('0.0')
+    .addTag('nonprofit')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
