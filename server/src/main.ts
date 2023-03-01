aa14109f38bc417b31c03efb8e2f2bf0b8c7a053
James D. Rogers <jd2rogers2@gmail.com>
2023-03-01 16:02:54 -0500

    swagger like mick jagger (#295)
    Co-authored-by: Esteban GS <26440947+esteban-gs@users.noreply.github.com>

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,21 +3,34 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
 
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import { AppModule } from './app.module';
 
 dotenv.config({ path: __dirname + '/.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.setGlobalPrefix('/api');
   app.enableCors({
     credentials: true,
-    // TODO get env related base url
-    origin: 'http://localhost:3000',
+    origin: process.env.FE_DOMAIN,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.use(cookieParser('secret_placeholder'));
+
+  const config = new DocumentBuilder()
+    .setTitle('Nonprofit Circle API')
+    .setDescription('Our API monolith')
+    .setVersion('0.0')
+    .addTag('nonprofit')
+    .addCookieAuth('NEH_is_cool')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
