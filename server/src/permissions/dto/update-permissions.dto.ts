ac3dd54707c4cfd11993132f68957c5b3191794e
Jessica Yu <yulejessica@gmail.com>
2023-03-16 14:16:31 -0700

    deleted src/permissions folder in server

--- server/src/permissions/dto/update-permissions.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePermissionsDto } from './create-permissions.dto';
-
-export class UpdatePermissionsDto extends PartialType(CreatePermissionsDto) {}
