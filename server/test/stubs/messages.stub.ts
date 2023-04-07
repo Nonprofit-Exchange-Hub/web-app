c4e6764bd534a88439f81e3225a95c6a6f3e904e
Skylar Salerno <skylar.salerno@gmail.com>
2023-04-07 10:25:20 -0400

    fix messages stub

--- server/test/stubs/messages.stub.ts
+++ server/test/stubs/messages.stub.ts
@@ -7,7 +7,8 @@ export const messageStub = (user?: User, transaction?: Transaction): Message => 
     id: 1,
     text: 'fakeMessage',
     created_date: new Date(2021, 11, 6),
-    user: user || new User(),
+    sending_user: user || new User(),
+    sending_org: null,
     transaction: transaction || new Transaction(),
   };
 };
