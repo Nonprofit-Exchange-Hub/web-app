b2f9596821678d592b4634e190d28d6ef43bd56d
Marvin Mante <marvin.mante@nextroll.com>
2023-05-12 17:08:22 -0700

    update organization controller and associated dtos

--- server/src/organizations/dto/create-organization.dto.ts
+++ server/src/organizations/dto/create-organization.dto.ts
@@ -1,53 +1,71 @@
 import { IsNotEmpty, IsOptional, Validate } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
 import { Interests } from '../../shared/interests.dto';
 import { InterestNamesIsArray, InterestsProps } from '../../shared/interests.validator';
 
 export class CreateOrganizationDto {
+  @ApiProperty()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   doing_business_as: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   nonprofit_classification: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   description: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   website: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   facebook?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   twitter?: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   instagram?: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   address: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   phone: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   city: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   state: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   zip_code: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   ein: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   image_url: string;
 
+  @ApiPropertyOptional()
   @IsOptional()
   @Validate(InterestsProps)
   @Validate(InterestNamesIsArray)
--- server/src/organizations/organizations.controller.ts
+++ server/src/organizations/organizations.controller.ts
@@ -11,6 +11,8 @@ import {
   HttpStatus,
   Query,
 } from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiOkResponse } from '@nestjs/swagger';
+
 import { OrganizationsService } from './organizations.service';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
 import { UpdateOrganizationDto } from './dto/update-organization.dto';
@@ -18,7 +20,6 @@ import { GetOrganizationDto } from './dto/get-organization.dto';
 import { Organization } from './entities/organization.entity';
 import { DeleteResult } from 'typeorm';
 import { PropublicaOrg } from './organizations.service';
-import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('organizations')
 @Controller('organizations')
@@ -26,6 +27,7 @@ export class OrganizationsController {
   constructor(private readonly organizationsService: OrganizationsService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create an organization.' })
   async create(@Body() createOrganizationDto: CreateOrganizationDto): Promise<Organization> {
     if (createOrganizationDto.categories) {
       const res = await this.organizationsService.validateOrgCategories(
@@ -47,21 +49,25 @@ export class OrganizationsController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Fetch organizations.' })
   find(@Query() getOrganizationDto: GetOrganizationDto): Promise<Organization[]> {
     return this.organizationsService.find(getOrganizationDto);
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Fetch organizations via ID.' })
   findOne(@Param('id') id: string): Promise<Organization> {
     return this.organizationsService.findOne(+id);
   }
 
   @Get('ein/:ein')
+  @ApiOperation({ summary: 'Fetch Propublica organization via EIN.' })
   getProPublicaOrg(@Param('ein') ein: string): Promise<PropublicaOrg> {
     return this.organizationsService.getProPublicaOrg(ein);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Update an organization.' })
   async update(
     @Param('id') id: string,
     @Body() updateOrganizationDto: UpdateOrganizationDto,
@@ -70,6 +76,7 @@ export class OrganizationsController {
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Delete an organization.' })
   remove(@Param('id') id: string): Promise<DeleteResult> {
     return this.organizationsService.remove(+id);
   }
