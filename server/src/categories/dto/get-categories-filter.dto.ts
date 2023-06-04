d39f64b39e135b290bb2a079776552629b3cfaad
Marvin Mante <marvin.mante@nextroll.com>
2023-06-04 13:16:02 -0700

    remove un-needed annotations from swagger

--- server/src/categories/dto/get-categories-filter.dto.ts
+++ server/src/categories/dto/get-categories-filter.dto.ts
@@ -1,14 +1,11 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
 import { IsOptional } from 'class-validator';
 
 import { CreateCategoryDto } from './create-category.dto';
 
 export class GetCategoriesDto extends CreateCategoryDto {
-  @ApiPropertyOptional()
   @IsOptional()
   id: number;
 
-  @ApiPropertyOptional()
   @IsOptional()
   name: string;
 }
