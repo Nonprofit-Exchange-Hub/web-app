860a82e9edb6a84e59d873fe86d3b7ff0adec156
Marvin Mante <marvin.mante@nextroll.com>
2023-06-22 07:13:21 -0700

    remove unnecessary decorators

--- server/src/messages/dto/create-message.dto.ts
+++ server/src/messages/dto/create-message.dto.ts
@@ -1,14 +1,11 @@
 import { IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
 
 import { Transaction } from '../../transactions/entities/transaction.entity';
 
 export class CreateMessageDto {
-  @ApiProperty()
   @IsNotEmpty()
   text: string;
 
-  @ApiProperty()
   @IsNotEmpty()
   transaction: Transaction;
 }
