d07d1d3d96f3c3f156b13af49c0d7963bd814467
Marvin Mante <marvin.mante@nextroll.com>
2023-06-23 12:42:03 -0700

    exclude properties from ReturnOrganizationDto

--- server/src/organizations/dto/return-organization.dto.ts
+++ server/src/organizations/dto/return-organization.dto.ts
@@ -1,8 +1,53 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional, Validate } from 'class-validator';
 
-import { CreateOrganizationDto } from './create-organization.dto';
+import { Interests } from '../../shared/interests.dto';
+import { InterestNamesIsArray, InterestsProps } from '../../shared/interests.validator';
 
-export class ReturnOrganizationDto extends CreateOrganizationDto {
+export class ReturnOrganizationDto {
   @IsNotEmpty()
   id: number;
+
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  doing_business_as: string;
+
+  @IsNotEmpty()
+  description: string;
+
+  @IsNotEmpty()
+  website: string;
+
+  @IsOptional()
+  facebook?: string;
+
+  @IsOptional()
+  twitter?: string;
+
+  @IsOptional()
+  instagram?: string;
+
+  @IsNotEmpty()
+  address: string;
+
+  @IsNotEmpty()
+  phone: string;
+
+  @IsNotEmpty()
+  city: string;
+
+  @IsNotEmpty()
+  state: string;
+
+  @IsNotEmpty()
+  zip_code: string;
+
+  @IsNotEmpty()
+  image_url: string;
+
+  @IsOptional()
+  @Validate(InterestsProps)
+  @Validate(InterestNamesIsArray)
+  categories?: Interests;
 }
