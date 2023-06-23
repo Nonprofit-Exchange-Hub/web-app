173152de449c659351a8b22d5aecd5c6d53410ea
Marvin Mante <marvin.mante@nextroll.com>
2023-06-23 12:29:43 -0700

    remove unused import

--- server/src/messages/messages.service.ts
+++ server/src/messages/messages.service.ts
@@ -6,7 +6,6 @@ import { User } from '../acccount-manager/entities/user.entity';
 import { Message } from './entities/message.entity';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { UpdateMessageDto } from './dto/update-message.dto';
-import { ReturnUserDto } from '../acccount-manager/dto/auth.dto';
 
 @Injectable()
 export class MessagesService {
