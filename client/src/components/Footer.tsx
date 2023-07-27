2b175c7f3bd4de9058fe22f1fbdd6aa77d2ef00f
Alex <92830192+EtoKruto@users.noreply.github.com>
2023-07-26 22:14:58 -0700

    updated the footer with the mockup provided, still need confirmation on correct links

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,17 +1,21 @@
-import Typography from '@mui/material/Typography';
+// import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { Theme } from '@mui/material/styles';
 import { makeStyles } from 'tss-react/mui';
-
-import StyledLink from './StyledLink';
+import logo from './../assets/GivingfulLogoPlum.svg';
 import routes from '../routes/routes';
+import { NavLink } from 'react-router-dom';
 
 const useStyles = makeStyles()((theme: Theme) => {
   return {
     main: {
       backgroundColor: 'white',
-      padding: theme.spacing(5),
+      padding: theme.spacing(3),
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      height: '100%',
     },
     linkBlock: {
       '& > *': {
@@ -21,15 +25,37 @@ const useStyles = makeStyles()((theme: Theme) => {
     header: {
       fontWeight: 'bold',
     },
-    bottom: {
-      marginTop: theme.spacing(4),
-    },
     footer: {
       boxShadow: '0px -2px 1em grey',
       position: 'relative',
       bottom: 0,
       left: 0,
       right: 0,
+    },
+    textBox: {
+      marginBottom: theme.spacing(2),
+      width: '550px',
+    },
+    text: {
+      fontSize: '16px',
+      lineHeight: '24px',
+      fontWeight: 400,
+      fontFamily: 'Poppins',
+      textAlign: 'center',
+    },
+    navLink: {
+      fontWeight: '600',
+      textDecoration: 'none',
+      color: 'black',
+      fontSize: '16px',
+      lineHeight: '24px',
+      marginRight: '21px',
+      fontFamily: 'Poppins',
+    },
+    logo: {
+      width: '175px',
+      height: '40px',
+      // marginBottom: theme.spacing(1),
     },
   };
 });
@@ -39,80 +65,39 @@ function Footer() {
 
   return (
     <footer className={classes.footer}>
-      <Grid className={classes.main} container>
-        <Grid container>
-          <Grid item xs={6} sm={8}>
-            <Typography className={classes.header} marginLeft={6}>
-              Tribio
-            </Typography>
-          </Grid>
-          <Grid
-            className={classes.linkBlock}
-            container
-            item
-            xs={3}
-            sm={2}
-            direction="column"
-            alignItems="flex-start"
-            paddingLeft={{ xs: 1, sm: 3 }}
-          >
-            <Box>
-              <Typography className={classes.header} align="left" gutterBottom>
-                Get to know us
-              </Typography>
-            </Box>
-            {/* <StyledLink to={routes.AboutUs.path}>About Us</StyledLink> */}
-            <Box>
-              <StyledLink to={routes.OurStory.path}>Our Story</StyledLink>
-            </Box>
-            <Box>
-              <StyledLink to={routes.HowItWorks.path}>How It works</StyledLink>
-            </Box>
-            <Box>
-              <StyledLink to={routes.ContactUs.path}>Contact Us</StyledLink>
-            </Box>
-          </Grid>
-          <Grid
-            className={classes.linkBlock}
-            container
-            item
-            xs={3}
-            sm={2}
-            direction="column"
-            alignItems="flex-start"
-            paddingLeft={{ xs: 1, sm: 3 }}
-          >
-            <Box>
-              <Typography className={classes.header} align="left" gutterBottom>
-                Resources
-              </Typography>
-            </Box>
-            <Box>
-              <StyledLink to={routes.TrustAndSafety.path}>Library</StyledLink>
-            </Box>
-            <Box>
-              <StyledLink to={routes.TrustAndSafety.path}>Trust and Safety</StyledLink>
-            </Box>
-            <Box>
-              <StyledLink to={routes.Help.path}>Help & FAQ</StyledLink>
-            </Box>
-          </Grid>
+      <Grid
+        className={classes.main}
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid item flexGrow={1} className={classes.textBox}>
+          <Box className={classes.text}>
+            We want to extend a special thank you to all those who work hard to support non-profit
+            organizations.
+          </Box>
         </Grid>
-        <Grid
-          className={classes.bottom}
-          container
-          item
-          sm={8}
-          xs={10}
-          justifyContent="space-around"
-          paddingTop={{ xs: 1, sm: 3 }}
-        >
-          {/* TODO Not sure if NEH 2021 is supposed to just be text, or a link.
-                    Leaving as text for now, as the name seems like it is changing anyway.*/}
-          <span>NEH 2021</span>
-          <StyledLink to={routes.TermsOfService.path}>Terms of Service</StyledLink>
-          <StyledLink to={routes.PrivacyPolicy.path}>Privacy Policy</StyledLink>
-          <StyledLink to={routes.CookiePolicy.path}>Cookie Policy</StyledLink>
+        <Grid item flexGrow={1}>
+          <Box>
+            <img className={classes.logo} alt="logo" src={logo}></img>
+          </Box>
+        </Grid>
+        <Grid item flexGrow={1}>
+          <Grid container item justifyContent="center">
+            <NavLink className={classes.navLink} to={routes.OurStory.path}>
+              Our Mission
+            </NavLink>
+            <NavLink className={classes.navLink} to={routes.HowItWorks.path}>
+              How It works
+            </NavLink>
+            <NavLink className={classes.navLink} to={routes.Help.path}>
+              FAQ
+            </NavLink>
+            <NavLink className={classes.navLink} to={routes.TermsOfService.path}>
+              Terms of Service
+            </NavLink>
+          </Grid>
         </Grid>
       </Grid>
     </footer>
