949436da3931fbb283f0a6f7aa61f79fa906abaf
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2023-09-09 22:06:12 -0700

    remove unused vars from server

--- server/src/shared/interests.validator.ts
+++ server/src/shared/interests.validator.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// this eslint rule is disabled because the number of arguments changes the method signature
 import {
   ValidatorConstraint,
   ValidatorConstraintInterface,
@@ -7,24 +9,23 @@ import { Interests } from './interests.dto';
 
 @ValidatorConstraint({ name: 'InterestsProps', async: false })
 export class InterestsProps implements ValidatorConstraintInterface {
-  validate(interests: Interests, args: ValidationArguments) {
+  validate(interests: Interests, _args: ValidationArguments) {
     const interestsKeys = Object.keys(interests);
     return interestsKeys.includes('names') && interestsKeys.length === 1;
   }
 
-  defaultMessage(args: ValidationArguments) {
+  defaultMessage(_args: ValidationArguments) {
     return 'interests must contain a names key';
   }
 }
 
 @ValidatorConstraint({ name: 'InterestNamesIsArray', async: false })
 export class InterestNamesIsArray implements ValidatorConstraintInterface {
-  validate(interests: Interests, args: ValidationArguments) {
-    const interestsKeys = Object.keys(interests);
+  validate(interests: Interests, _args: ValidationArguments) {
     return Array.isArray(interests.names) && interests.names.every((k) => typeof k === 'string');
   }
 
-  defaultMessage(args: ValidationArguments) {
+  defaultMessage(_args: ValidationArguments) {
     return 'interests names must contain a valid array of strings';
   }
 }
