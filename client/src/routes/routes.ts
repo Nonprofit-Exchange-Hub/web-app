1275489c8ec5b46c5fa97e6abc9ab9a93483c473
esteban-gs <26440947+esteban-gs@users.noreply.github.com>
2023-11-17 16:17:29 -0800

    login modal from route navigation

--- client/src/routes/routes.ts
+++ client/src/routes/routes.ts
@@ -26,6 +26,7 @@ import SetNewPassword from '../components/Users/Auth/SetNewPassword';
 import CookiePolicy from '../views/CookiePolicy';
 import TempChat from '../views/TempChat';
 import EmailVerification from '../views/EmailVerification';
+import Login from '../views/Login';
 
 type RouteMap = {
   [componentName: string]: {
@@ -90,6 +91,11 @@ const routes: RouteMap = {
     component: Signup,
     roles: [],
     path: '/signup',
+  },
+  Login: {
+    component: Login,
+    roles: [],
+    path: '/login',
   },
   SignupCitizen: {
     component: SignupCitizen,
