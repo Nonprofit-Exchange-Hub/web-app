185fdaba91190ff5a32a6f4b2d64b6b3391a1d43
Esteban Ginocchio Silva <26440947+esteban-gs@users.noreply.github.com>
2024-03-24 16:11:52 -0700

    378/cleaner signup flow (#383)
    * accounts/signup endpoint
    
    * Narrow account creation scope, enable update with more fields
    
    * test account manager controller changes
    
    * add login api body type
    
    * update createUserDto stub
    
    * integration test update endpoint
    
    * Removed unused import
    
    * Update user with id from cookie jwt
    
    * Add update method return type
    
    * imported ResetPasswordDto
    
    * imported LoginDto
    
    ---------
    
    Co-authored-by: Dennis Wang <88702296+unsolvedanomaly@users.noreply.github.com>
    Co-authored-by: denniswangcodes <denniswang.self@gmail.com>

--- server/src/acccount-manager/dto/create-user.dto.ts
+++ server/src/acccount-manager/dto/create-user.dto.ts
@@ -1,6 +1,4 @@
-import { IsEmail, IsNotEmpty, IsBoolean, IsOptional, Validate } from 'class-validator';
-import { InterestsProps, InterestNamesIsArray } from '../../shared/interests.validator';
-import { Interests } from '../../shared/interests.dto';
+import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
@@ -15,24 +13,4 @@ export class CreateUserDto {
 
   @IsNotEmpty()
   password: string;
-
-  @IsOptional()
-  bio?: string;
-
-  @IsNotEmpty()
-  city: string;
-
-  @IsNotEmpty()
-  state: string;
-
-  @IsNotEmpty()
-  zip_code: string;
-
-  @IsBoolean()
-  email_notification_opt_out: boolean;
-
-  @IsOptional()
-  @Validate(InterestsProps)
-  @Validate(InterestNamesIsArray)
-  interests?: Interests;
 }
